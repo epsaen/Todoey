8888b5b9dfb23be6cf5be0730657f849a3da5770
Saen Jindamanee <saen.ehput@gmail.com>
2019-02-18 00:20:48 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Saen Eh Put on 17/02/2019.
+//  Copyright © 2019 Saen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
